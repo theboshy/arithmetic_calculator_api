330b5b4eb251912e8085f31071d8efd5ea41970a
Peter G. Lobo <akumaokami10@gmail.com>
2023-04-26 14:09:24 -0500

    createa rchitecture and patter definition for backend
    - using serverless

---
+++ src/functions/index.ts
@@ -0,0 +1,1 @@
+export { test } from './v1/test';
---
+++ src/functions/v1/test/handler.ts
@@ -0,0 +1,14 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import schema from '../../../common/v1/schemas/schema';
+
+const test: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  return formatJSONResponse({
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    event,
+  });
+};
+
+export const testHandler = middyfy(test);
